eeab687a2ffda65b9101aec8ca762ae26aa4eee3
Rayudu Bhargav <bhargavrayudu1502@gmail.com>
2026-03-14 10:57:26 +0530

    Add files via upload

---
+++ Week4/Day5/Bank Account with Encapsulation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Week4Day5
+{
+    class BankAccount
+    {
+        private double balance;
+        public void Deposit(double amount)
+        {
+            balance = balance + amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount <= balance)
+            {
+                balance = balance - amount;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient Balance");
+            }
+        }
+
+        public double GetBalance()
+        {
+            return balance;
+        }
+    }
+
+    internal class Bank_Account_with_Encapsulation
+    {
+        static void Main()
+        {
+            BankAccount account = new BankAccount();
+
+            account.Deposit(1500);
+            account.Withdraw(500);
+
+            Console.WriteLine("Current Balance = " + account.GetBalance());
+        }
+    }
+}
+
+
