cb17722ba4b6a0bcd919f94f8f434c90725ca8a2
Rayudu Bhargav <bhargavrayudu1502@gmail.com>
2026-03-16 18:25:59 +0530

    Add files via upload

---
+++ Day5/Employee Salary Calculator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Week5Day1
+{    
+        class Employee
+        {
+            public string Name { get; set; }
+            public double BaseSalary { get; set; }
+
+            public virtual double CalculateSalary()
+            {
+                return BaseSalary;
+            }
+        }
+
+        class Manager : Employee
+        {
+            public override double CalculateSalary()
+            {
+                return BaseSalary + (BaseSalary * 0.20);
+            }
+        }
+
+        class Developer : Employee
+        {
+            public override double CalculateSalary()
+            {
+                return BaseSalary + (BaseSalary * 0.10);
+            }
+        }
+
+        class Employee_Salary_Calculator
+    {
+            static void Main()
+            {
+                double baseSalary = 50000;
+
+                Employee manager = new Manager();
+                manager.Name = "Manager";
+                manager.BaseSalary = baseSalary;
+
+                Employee developer = new Developer();
+                developer.Name = "Developer";
+                developer.BaseSalary = baseSalary;
+
+                Console.WriteLine("Manager Salary = " + manager.CalculateSalary());
+                Console.WriteLine("Developer Salary = " + developer.CalculateSalary());
+            }
+        }
+    }
---
+++ Day5/Vehicle Rental System.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace Week5Day1
+{
+    class Vehicle
+    {
+        private string brand;
+        private double rentalRatePerDay;
+        public string Brand
+        {
+            get { return brand; }
+            set { brand = value; }
+        }
+
+        public double RentalRatePerDay
+        {
+            get { return rentalRatePerDay; }
+            set { rentalRatePerDay = value; }
+        }
+        public virtual double CalculateRental(int days)
+        {
+            if (days <= 0)
+            {
+                Console.WriteLine("Invalid number of rental days.");
+                return 0;
+            }
+
+            return RentalRatePerDay * days;
+        }
+    }
+
+    class Car : Vehicle
+    {
+        private const double InsuranceCharge = 500;
+        public override double CalculateRental(int days)
+        {
+            if (days <= 0)
+            {
+                Console.WriteLine("Invalid number of rental days.");
+                return 0;
+            }
+
+            double total = (RentalRatePerDay * days) + InsuranceCharge;
+            return total;
+        }
+    }
+
+    class Bike : Vehicle
+    {
+        public override double CalculateRental(int days)
+        {
+            if (days <= 0)
+            {
+                Console.WriteLine("Invalid number of rental days.");
+                return 0;
+            }
+
+            double total = RentalRatePerDay * days;
+            double discount = total * 0.05;
+
+            return total - discount;
+        }
+    }
+
+    internal class Vehicle_Rental_System
+    {
+        static void Main()
+        {
+            Vehicle vehicle;
+
+            int days = 3;
+
+            vehicle = new Car();
+            vehicle.Brand = "Kia";
+            vehicle.RentalRatePerDay = 2000;
+
+            double totalRental = vehicle.CalculateRental(days);
+
+            Console.WriteLine("Total Rental = " + totalRental);
+        }
+    }
+}
