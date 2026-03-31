9f8052764754af0cef78313c2dcc5ad087820e95
Rayudu Bhargav <bhargavrayudu1502@gmail.com>
2026-03-31 20:12:33 +0530

    Add files via upload

---
+++ Week7/Day1/ContactManagementWebApplication/Controllers/ContactController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Contact_Management_Web_Application.Models;
+
+namespace Contact_Management_Web_Application.Controllers
+{
+    [Route("contact")]
+    public class ContactController : Controller
+    {
+        static List<ContactInfo> contacts = new List<ContactInfo>();
+
+        [HttpGet("show")]
+        public IActionResult ShowContacts()
+        {
+            return View(contacts);
+        }
+
+        [HttpGet("get")]
+        public IActionResult GetContactById(int id)
+        {
+            var contact = contacts.FirstOrDefault(c => c.ContactId == id);
+
+            if (contact == null)
+                ViewBag.Message = "Contact not found";
+
+            return View(contact);
+        }
+
+        [HttpGet("add")]
+        public IActionResult AddContact()
+        {
+            return View();
+        }
+
+        [HttpPost("add")]
+        public IActionResult AddContact(ContactInfo contactInfo)
+        {
+            contacts.Add(contactInfo);
+            return RedirectToAction("ShowContacts");
+        }
+    }
+}
