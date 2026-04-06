65660c47c1a438ff5c2413b8189004ca909abaed
Rayudu Bhargav <bhargavrayudu1502@gmail.com>
2026-04-06 22:48:36 +0530

    Add files via upload

---
+++ Week7/Day2/Feedback Form with Conditional Message/Controllers/FeedbackController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FeedbackApp.Controllers
+{
+    [Route("feedback")]
+    public class FeedbackController : Controller
+    {
+ 
+        [HttpGet("form")]
+        public IActionResult Form()
+        {
+            return View();
+        }
+
+        [HttpPost("form")]
+        public IActionResult Form(string name, string comments, int rating)
+        {
+            string message;
+
+            if (rating >= 4)
+            {
+                message = "Thank You for your valuable feedback!";
+            }
+            else
+            {
+                message = "We will improve based on your feedback.";
+            }
+
+            ViewData["Message"] = message;
+            ViewData["Name"] = name;
+
+            return View();
+        }
+    }
+}
