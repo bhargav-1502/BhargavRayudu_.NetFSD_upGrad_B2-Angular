3589ffd1f0d492c3cd6ca80cdeadf7adc8fcc0af
Rayudu Bhargav <bhargavrayudu1502@gmail.com>
2026-04-23 17:11:54 +0530

    Add files via upload

---
+++ Week10/Day2/ContactManagementApi/ContactManagementApi/Program.cs
@@ -0,0 +1,25 @@
+using ContactManagementApi.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<IContactService, ContactService>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
